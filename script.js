b8f26a3a14dfb1ac7a480dfd7abf307864174bad
fanynabilah <fanynabilah@gmail.com>
2022-02-25 02:55:35 +0700

    mengubah layout result

--- script.js
+++ script.js
@@ -47,7 +47,7 @@ const getChar = async (keyword) => {
               <div class="result-element">
                 <div class="character-element">
                   <h5 class="character-name">Studio Ghibli Character: <strong><em>${name}</em></strong></h5>
-                  <table class="result-box">
+                  <table class="result-box" cellpadding="4">
                     <tr>
                       <td>Age</td>
                       <td>:</td>
@@ -69,21 +69,30 @@ const getChar = async (keyword) => {
                       <td>${hairColor}</td>
                     </tr>
                     <tr>
-                      <td>Appears on</td>
-                      <td>:</td>
-                      <td>${title} (${oriTitle})</td>
+                      <td valign="top">Appears on</td>
+                      <td valign="top">:</td>
+                      <td valign="top">${title}<br>${oriTitle}</td>
                     </tr>
                   </table>
                 </div>
+                <p class="details-bar">Movie Details</p>
                 <div class="movie-element">
-                  <p class="details-bar">Movie Details</p>
-                  <table class="movie-box" cellpadding="15">
+                  <table class="movie-box">
                     <tr>
                       <td valign="top"><img src="${image}" width="200"></td>
-                      <td valign="top">
-                        <strong>${title} (${oriTitle})</strong><br>
+                    </tr>
+                    <tr>
+                      <td>
+                        <strong><br>${title}<br>${oriTitle}</strong>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>
                         ${year}<br><br>
-                        Description :<br>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>
                         ${desc}
                       </td>
                     </tr>
@@ -101,84 +110,10 @@ const getChar = async (keyword) => {
   }
 };
 
-/*const displayChar = (characters) => {
-  const charDiv = document.getElementById("characters");
-
-  let listOfCharElement = "";
-  for (let i = 0; i < ghibliChar.length; i++) {
-    const ghibli = ghibliChar[i];
-
-    fetch(ghibliChar[i].films)
-      .then((response) => response.json())
-      .then((response) => {
-        const name = ghibli.name;
-        const gender = ghibli.gender;
-        const eyeColor = ghibli.eye_color;
-        const hairColor = ghibli.hair_color;
-        const title = response.title;
-        const oriTitle = response.original_title;
-        const image = response.image;
-        const desc = response.description;
-        const year = response.release_date;
-        let age;
-        if (ghibli.age == "") {
-          age = "NA";
-        } else {
-          age = ghibli.age;
-        }
-
-        const movieResult = `
-              <div class="result-element">
-                <div class="character-element">
-                  <h5 class="character-name">Studio Ghibli Character: <strong><em>${name}</em></strong></h5>
-                  <table class="result-box">
-                    <tr>
-                      <td>Age</td>
-                      <td>:</td>
-                      <td>${age}</td>
-                    </tr>
-                    <tr>
-                      <td>Gender</td>
-                      <td>:</td>
-                      <td>${gender}</td>
-                    </tr>
-                    <tr>
-                      <td>Eye Color</td>
-                      <td>:</td>
-                      <td>${eyeColor}</td>
-                    </tr>
-                    <tr>
-                      <td>Hair Color</td>
-                      <td>:</td>
-                      <td>${hairColor}</td>
-                    </tr>
-                    <tr>
-                      <td>Appears on</td>
-                      <td>:</td>
-                      <td>${title} (${oriTitle})</td>
-                    </tr>
-                  </table>
-                </div>
-                <div class="movie-element">
-                  <img src="${image}" width="100">
-                  <p>${desc}</p>                    
-                  <p>Year Release: ${year}</p>
-                </div>
-              </div>
-              `;
-
-        listOfCharElement += movieResult;
-      });
-  }
-  charDiv.innerHTML = listOfCharElement;
-};*/
-
 const findChar = async () => {
   const keyword = document.getElementById("character-name").value;
   if (keyword == "") {
     console.log("Character Not Found");
   }
   const characters = await getChar(keyword);
-  //displayChar(characters);
-  //displayChar(characters.results);
 };
